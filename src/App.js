408c2898e6743acdf5133ac4d522d7a6dede40af
moghatt <mohammad_ghatali_kh@yahoo.com>
2024-01-08 16:39:28 +1030

    add detail component with functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
+import { Routes, BrowserRouter, Route } from 'react-router-dom';
 import './App.scss';
 import Header from './components/header/Header';
 import Main from './components/main/Main';
+import Details from './components/content/details/Details';
 
 function App() {
   return (
     <>
-      <Header />
-      <div className="app">
-        <Main />
-      </div>
+      <BrowserRouter>
+        <Header />
+        <div className="app">
+          <Routes>
+            <Route exact path="/" element={<Main />} />
+            <Route exact path="/:id/:name/details" element={<Details />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </>
   );
 }
